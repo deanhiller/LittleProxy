0d8aa54287635e800fa8ba09a0c94dc6f9096e27
Dean Hiller <dean@xsoftware.biz>
2012-04-25 19:24:41 -0600

    adding screen scraper here

--- src/main/java/com/alvazan/proxy/MainForScraping.java
+++ src/main/java/com/alvazan/proxy/MainForScraping.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.littleshoot.proxy.DefaultHttpProxyServer;
-
-import com.alvazan.proxy.MainForProxy.TimeNowImpl;
 import com.alvazan.proxy.scraping.WebPageScraper;
 
 public class MainForScraping {
@@ -18,7 +15,11 @@ public class MainForScraping {
 		props.put("hibernate.connection.url", "jdbc:h2:mem:test");
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("h2", props);
 
-		WebPageScraper scraper = new WebPageScraper(factory, args[0]);
+		String url = "http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=tax+accountants&aq=&sll=39.737567,-104.984718&sspn=0.570256,0.862427&vpsrc=6&g=denver,+CO&ie=UTF8&t=m&fll=39.837014,-105.037537&fspn=0.552562,1.047821&st=113699889012030469848&rq=1&ev=zo&split=1&jsv=408d&vps=11&sa=N&start=40";
+		if(args.length > 0)
+			url = args[0];
+		
+		WebPageScraper scraper = new WebPageScraper(factory, url);
 		scraper.start();
 	}
 }
--- src/main/java/com/alvazan/proxy/scraping/WebPageScraper.java
+++ src/main/java/com/alvazan/proxy/scraping/WebPageScraper.java
@@ -1,15 +1,25 @@
 package com.alvazan.proxy.scraping;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.alvazan.proxy.VisitedFilter;
 import com.alvazan.proxy.db.Domain;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class WebPageScraper {
 
+	private static final Logger log = LoggerFactory.getLogger(WebPageScraper.class);
 	private EntityManagerFactory factory;
 	private String urlToStart;
 
@@ -20,13 +30,18 @@ public class WebPageScraper {
 
 	public void start() {
 		String url = urlToStart;
-		
+		WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
+
+		int count = 0;
 		while(url != null) {
-			ScrapedInformation info = findPagesListOfDomains(url);
+			log.info("count = "+ count);
+			ScrapedInformation info = findPagesListOfDomains(webClient, url);
 			saveDomainsToDatabase(info.getDomains());
 			url = info.getNextPageUrl();
+			count++;
 		}
 		
+		webClient.closeAllWindows();
 	}
 
 	private void saveDomainsToDatabase(List<String> domains) {
@@ -46,11 +61,43 @@ public class WebPageScraper {
 
 	//Main screen scrape goes here..and add each domain to the ScrapeInformation.java object
 	//and then add the next pages url as well
-	private ScrapedInformation findPagesListOfDomains(String url) {
+	private ScrapedInformation findPagesListOfDomains(WebClient webClient, String url) {
+		try {
+			return findPagesListOfDomainsImpl(webClient, url);
+		} catch (FailingHttpStatusCodeException e) {
+			throw new RuntimeException(e);
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	private ScrapedInformation findPagesListOfDomainsImpl(WebClient webClient, String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		//http://maps.google.com/maps?q=tax+accountants&hl=en&ll=39.773714,-104.959259&spn=0.553071,1.047821&sll=39.737567,-104.984718&sspn=0.570256,0.862427&t=m&fll=39.837014,-105.037537&fspn=0.552562,1.047821&z=10
+		//http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=tax+accountants&aq=&sll=39.737567,-104.984718&sspn=0.570256,0.862427&vpsrc=6&g=denver,+CO&ie=UTF8&t=m&fll=39.837014,-105.037537&fspn=0.552562,1.047821&st=113699889012030469848&rq=1&ev=zo&split=1&jsv=408d&vps=11&sa=N&start=10
+		//http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=tax+accountants&aq=&sll=39.737567,-104.984718&sspn=0.570256,0.862427&vpsrc=6&g=denver,+CO&ie=UTF8&t=m&fll=39.837014,-105.037537&fspn=0.552562,1.047821&st=113699889012030469848&rq=1&ev=zo&split=1&jsv=408d&vps=11&sa=N&start=20
+		HtmlPage page = webClient.getPage(url);
+		String asXml = page.asXml();
+		log.info("xml="+asXml);
+		
+		findExtensions(asXml, ".net");
+		findExtensions(asXml, ".com");
+		findExtensions(asXml, ".biz");
+		
+		if(asXml.contains("Advance America"))
+			log.info("we got it");
+		
 		ScrapedInformation info = new ScrapedInformation();
-		//TODO: fix this..
 		info.getDomains().add("somedomain that was scraped");
 		return null;
+	}
+
+	private void findExtensions(String asXml, String postfix) {
+		int indexOf = asXml.indexOf(postfix);
+		int begin = indexOf - 100;
+		int end = indexOf + postfix.length();
+		String url = asXml.substring(begin, end);
+		log.info("url="+url);
 	}
 
 }
